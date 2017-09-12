ea0747ce5546dc0c14c961115f8a15227483ab88
liusheng613 <liusheng613@github.com>
2017-09-12 23:10:41 +0800

    调试MergeSort 结束

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -31,21 +31,29 @@ void Sort<Type>::InsertSort(DataList<Type> & sortData)
 template <typename Type>
 void Sort<Type>::MergeSort(DataList<Type> & sortData)
 {
-    _MergeSort(sortData,0,sortData.size());
+    //must be size()-1,use index for 0 to sortData.size()-1
+    //otherwise the Merge can be merge one element,like Merge(0,0,1)
+    _MergeSort(sortData,0,sortData.size()-1);
 }
 
 template <typename Type>
 void Sort<Type>::_MergeSort(DataList<Type> & sortData,int begin,int end)
 {
     int pivotpos = 0;
-    std::cout<<"in _MergeSort before if begin="<<begin<<",end="<<end<<std::endl;
+    // std::cout<<"in _MergeSort before if begin="<<begin<<",end="<<end<<std::endl;
     if(begin < end)
     {
         pivotpos = (begin + end)/2;
-        std::cout<<"in _MergeSort pivotpos="<<pivotpos<<std::endl;
+        // std::cout<<"in _MergeSort pivotpos="<<pivotpos<<std::endl;
         _MergeSort(sortData,begin,pivotpos);
         _MergeSort(sortData,pivotpos+1,end);
         Merge(sortData,begin,pivotpos,end);
+        // std::cout<<"---------------------------"<<std::endl;
+        // for(int i = begin;i < end; ++i)
+        // {
+        //     std::cout<<"sortData["<<i<<"]="<<sortData[i]<<std::endl;
+        // }
+        // std::cout<<"---------------------------"<<std::endl;
     }
 }
 
@@ -53,9 +61,9 @@ void Sort<Type>::_MergeSort(DataList<Type> & sortData,int begin,int end)
 template <typename Type>
 void Sort<Type>::Merge(DataList<Type> & sortData,int begin,int pivotpos,int end)
 {
-    std::cout<<"in Merge begin="<<begin<<",pivotpos="<<pivotpos<<",end="<<end<<std::endl;
+    // std::cout<<"in Merge begin="<<begin<<",pivotpos="<<pivotpos<<",end="<<end<<std::endl;
     int n1 = pivotpos - begin + 1;
-    int n2 = end - pivotpos -1 ;
+    int n2 = end - pivotpos;
 
     DataList<int> L(n1+1);
     DataList<int> R(n2+1);
@@ -72,12 +80,15 @@ void Sort<Type>::Merge(DataList<Type> & sortData,int begin,int pivotpos,int end)
         R[j] = sortData[pivotpos + 1 + j];
     }
 
+    //set the last element as a guard
     L[n1] = 65535;
     R[n2] = 65535;
     i = 0;
     j = 0;
 
-    for(int k = begin;k<end;++k)
+    //here must be <= not <,because here the 
+    //end is the index of SortData,not the length
+    for(int k = begin;k<=end;++k)
     {
         if(L[i] <= R[j])
         {
@@ -89,7 +100,6 @@ void Sort<Type>::Merge(DataList<Type> & sortData,int begin,int pivotpos,int end)
             sortData[k] = R[j];
             ++j;
         }
-        std::cout<<"in Merge sortData["<<k<<"]="<<sortData[k]<<std::endl;
     }
 }
 
